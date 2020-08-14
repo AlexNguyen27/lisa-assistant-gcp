95535cc6c9f8295e7f7dbbafdba67855d4394155
nguyentangtrung23061998 <nguyentangtrung23061998@gmail.com>
2020-08-14 10:01:34 +0700

    Fix API Weather

--- weather.py
+++ weather.py
@@ -3,15 +3,21 @@ from text_to_speech import read_text
 
 # Searching for weather
 def weather():
-    url = 'https://openweathermap.org/data/2.5/weather/?appid=b6907d289e10d714a6e88b30761fae22&id=1566083&units=metric'
-    res = requests.get(url)
+    # url = 'https://openweathermap.org/data/2.5/weather/?appid=b6907d289e10d714a6e88b30761fae22&id=1566083&units=metric'
+    url = 'https://api.openweathermap.org/data/2.5/weather?id=1566083&appid=a395d3d89b3fc57be94693534c103fe4'
+    print('url: ' + url)
+
+    payload = {}
+    headers = {}
+    res = requests.request("GET", url, headers=headers, data = payload)
+    res.text.encode('utf8')
+
     data = res.json()
     temp = data['main']['temp']
     description = data['weather'][0]['description']
     name = data['name']
     humidity = data['main']['humidity']
-    print(name)
     read_text('Today in ' + name.replace("Thanh pho", "") + "city" + ', Temperature is: {} degree celcius'.format(
         temp) + ', Humidity : {}'.format(humidity) + ', Mostly: {}'.format(description))
 
-# weather()
+weather()
